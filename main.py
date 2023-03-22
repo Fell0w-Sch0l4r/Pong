197cbe5a37f3726436b725a0dfb91846a37b4acb
Ângel Cagiza <angelcagiza@gmail.com>
2023-03-22 22:13:03 +0000

    add ball object to screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from time import sleep
 
 
 screen = Screen()
@@ -11,6 +13,8 @@ screen.tracer(0)
 
 r_paddle = Paddle()
 l_paddle = Paddle(side="left")
+
+ball = Ball()
 
 
 
@@ -24,7 +28,9 @@ screen.onkey(key="w", fun=l_paddle.up)
 screen.onkey(key="s ", fun=l_paddle.down)
 
 while True:
+    sleep(0.1)
     screen.update()
+    ball.move()
 
 
 
