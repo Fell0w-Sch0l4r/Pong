94af5de53be9f68f249939a4ecb6e03284375544
Ângel Cagiza <angelcagiza@gmail.com>
2023-03-22 21:44:54 +0000

    refactor code to use paddle class

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 
 screen = Screen()
 screen.bgcolor("black")
+screen.title("PONG")
 screen.setup(width=800, height=600)
+screen.tracer(0)
+
+
+paddle = Paddle()
+pc = Paddle(player="pc")
+
+
+
+    
+
+
+screen.listen()
+screen.onkey(key="Up", fun=paddle.up)
+screen.onkey(key="Down", fun=paddle.down)
+
+while True:
+    screen.update()
+
 
 
 screen.exitonclick()
